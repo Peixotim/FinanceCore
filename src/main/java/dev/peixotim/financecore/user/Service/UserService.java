43cd51e2f2ece32839e267ea1c77ecabcb9d51fb
Peixotim <pedropeixotovz@gmail.com>
2025-10-24 09:51:06 -0300

    Feat: Adiciona a nova funcionalidade para alterar o tipo do usuario

--- src/main/java/dev/peixotim/financecore/user/Service/UserService.java
+++ src/main/java/dev/peixotim/financecore/user/Service/UserService.java
@@ -5,8 +5,8 @@ import dev.peixotim.financecore.user.DTOs.UserRequest;
 import dev.peixotim.financecore.user.DTOs.UserResponse;
 import dev.peixotim.financecore.user.Mapper.UserMapper;
 import dev.peixotim.financecore.user.Mapper.UserRequestResponseMapper;
+import dev.peixotim.financecore.user.Model.UserType;
 import dev.peixotim.financecore.user.Repository.UserRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -80,4 +80,14 @@ public class UserService {
 
     }
 
+    public UserResponse modifyUserType(String email , UserType type){
+        var find = repository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("We were unable to find any user with this email in our database"));
+        find.setUserType(type);
+        repository.save(find);
+        var toDTO = mapper.toDTO(find);
+        var response = mapperRes.toResponse(toDTO);
+
+        return response;
+
+    }
 }
